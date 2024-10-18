a556de8da0fb3ed8be4423b1bd8a83cafd1dfe5b
garvitsharma780 <sharmagarvit780@gmail.com>
2024-10-18 11:53:35 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      Discover a day filled with exciting activities, inspiring speakers, and vibrant networking opportunities. Whether you're here to learn, connect, or simply have fun, this event has something for everyone.
+
+Don't miss out on exclusive sessions, live performances, and more! Secure your spot today and be part of something extraordinary.
+      </p>
+    </section>
+  );
+};
+
+export default About;
